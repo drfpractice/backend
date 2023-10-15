b87042f3ac09459afc444c3be7fdbd2719ec56f8
Magomed-R <magomed-radzhabov@internet.ru>
2023-10-15 16:35:26 +0400

    create routes of getting and creating Students

--- lessonapp/urls.py
+++ lessonapp/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("demo/", include("authentication.urls"))
+    path("admin/", admin.site.urls),
+    path("", include('authentication.urls'))
 ]
